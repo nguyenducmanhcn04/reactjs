24ae631ff9571f14246450f0bc7093ffa6885b8f
Nguyễn Đức Mạnh <nguyenducmanh026@gmail.com>
2025-02-22 19:12:14 +0700

    arrow funcition

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,12 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0);
+  const myFunction = () => {
+    console.log("Hello from myFunction");
+  };
+  myFunction();
 
   return (
     <>
@@ -30,6 +34,6 @@ function App() {
       </p>
     </>
   );
-}
+};
 
 export default App;
