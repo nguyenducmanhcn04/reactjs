6a299af9f50bb272225dd6ba7001d8daaa026279
Nguyễn Đức Mạnh <nguyenducmanh026@gmail.com>
2025-02-24 00:54:37 +0700

    DOM event

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,10 +1,20 @@
 const TodoNew = (props) => {
   const { addNewTodo } = props;
+
+  const handleClick = () => {
+    alert("Click me");
+  };
+  const handleOnchange = (name) => {
+    console.log(">>>> handleOnchange", name);
+  };
   //   addNewTodo("manh");
   return (
     <div className="todo-new">
-      <input type="text" />
-      <button>Add Todo</button>
+      <input
+        type="text"
+        onChange={(event) => handleOnchange(event.target.value)}
+      />
+      <button onClick={handleClick}>Add Todo</button>
     </div>
   );
 };
