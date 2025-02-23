c8335112babb7db94cf6122fd29916b76fbe256e
Nguyễn Đức Mạnh <nguyenducmanh026@gmail.com>
2025-02-23 17:22:03 +0700

    use bien so with jsx

--- src/components/learn/Mycomponent.jsx
+++ src/components/learn/Mycomponent.jsx
@@ -2,10 +2,18 @@
 
 import "./style.css";
 const MyComponent = () => {
+  // const hoidanit = 24;
+  // const hoidanit = "hoidanit";
+  const hoidanit = {
+    name: "Nguyen Duc Manh",
+    age: 24,
+  };
+
   return (
     // fragment
     <>
-      <div> Nguyen Duc Manh</div>
+      <div> {JSON.stringify(hoidanit)} Nguyen Duc Manh</div>
+      <div>{console.log("nguyen duc manh")}</div>
       <div className="child" style={{ borderRadius: "10px" }}>
         Bac Giang
       </div>
